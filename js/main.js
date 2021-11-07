56ae628e0f5cb5432b0d79f11a985b3ed5d391db
filosof777 <olimjonkamalov@mail.ru>
2021-11-07 16:50:02 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,57 @@
-let elNavList = document.querySelector('.nav__list');
-let elHeaderBtn = document.querySelector('.header__btn');
+let elInput = document.querySelector('.passw');
+let elEyeBtn = document.querySelector('.header__password-btn');
+let n=0
 
-elHeaderBtn.addEventListener('click', () => {
-  elNavList.classList.toggle('show-menu');
-});
+elEyeBtn.addEventListener('click', ()=>{
+  if(n==0){
+    elInput.type = "text"
+    n=1
+    // document.getElementById("eye").className = "bi bi-eye-fill";
+  }
+  else{
+    elInput.type = "password"
+    n=0
+    // document.getElementById("eye").className = "bi bi-eye-slash-fill";
+  }
+})
 
+// slide
+
+var tab; // заголовок вкладки
+var tabContent; // блок содержащий контент вкладки
+
+
+window.onload=function() {
+    tabContent=document.getElementsByClassName('tabContent');
+    tab=document.getElementsByClassName('tab');
+    hideTabsContent(1);
+}
+
+document.getElementById('tabs').onclick= function (event) {
+    var target=event.target;
+    if (target.className=='tab') {
+       for (var i=0; i<tab.length; i++) {
+           if (target == tab[i]) {
+               showTabsContent(i);
+               break;
+           }
+       }
+    }
+}
+
+function hideTabsContent(a) {
+    for (var i=a; i<tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add("hide");
+        tab[i].classList.remove('whiteborder');
+    }
+}
+
+function showTabsContent(b){
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('whiteborder');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
